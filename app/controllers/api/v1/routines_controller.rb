9331c0ec115da101c3d7611fc827ca4c4507b01e
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-07-07 15:37:37 -0400

    fix typos

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::RoutinesController < ApplicationController
   def index
-    @routines = Routine.all
-    render json: @routines
+    routines = Routine.all
+    render json: routines
   end
 
   def user_routines
-    @routines = Routine.all.select { |r| r.user_id  == session[:user_id].to_i }
-    render json: @routines
+    routines = Routine.all.select { |r| r.user_id  == session[:user_id].to_i }
+    render json: routines
   end
 
   def show
-    @routine = Routine.find(params[:id])
-    render json: @routine
+    routine = Routine.find(params[:id])
+    render json: routine
   end
 
   def create
-    @routine = Routine.create(user_params)
-    render json: @user
+    routine = Routine.create(routines_params)
+    render json: routine
   end
 
   def update
-    @routine = Routine.find(params[:id])
-    if @routine
-      @routine.update(user_params)
-      render json: @routine
+    routine = Routine.find(params[:id])
+    if routine
+      routine.update(routines_params)
+      render json: routine
     end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email)
+  def routines_params
+    params.require(:routine).permit(:name, :email)
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,31 +1,31 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users
   end
 
   def show
-    @user = User.find(params[:id])
-    render json: @user
+    user = User.find(params[:id])
+    render json: user
   end
 
   def create
-    @user = User.create(user_params)
-    render json: @user
+    user = User.create(user_params)
+    render json: user
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user
-      @user.update(user_params)
-      render json: @user
+    user = User.find(params[:id])
+    if user
+      user.update(user_params)
+      render json: user
     end
   end
 
   def find_user
-    @user = User.find_by(username: params[:username] )
-    if @user
-      session[:user_id] = @user.id
+    user = User.find_by(username: params[:username] )
+    if user
+      session[:user_id] = user.id
     end
   end
 
